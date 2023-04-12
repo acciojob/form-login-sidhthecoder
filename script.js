43444fa84a34accf498508b8d2357723b63d53d2
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-04-12 16:09:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
-
+	let fname=document.getElementById("fname").value;
+	let lname=document.getElementById("lname").value;
+	alert(fname+" "+lname);
 }
+
+let submit=document.getquerySelector('input').value;
+submit.addEventListener('onclick',getFormvalue);
